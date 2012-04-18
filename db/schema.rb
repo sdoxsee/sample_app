95caa371462a80b2c68e555b813699dea7ea602c
Stephen Doxsee <stephen@doxsee.org>
2012-04-18 16:30:31 -0400

    Add Micropost model and associate to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120418173838) do
+ActiveRecord::Schema.define(:version => 20120418185008) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "user_id"
+  end
+
+  add_index "microposts", ["user_id", "created_at"], :name => "index_microposts_on_user_id_and_created_at"
 
   create_table "users", :force => true do |t|
     t.string   "name"
